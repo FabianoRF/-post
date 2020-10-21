77bac45cf6affa5eb3efd8bbfcb91555153b389f
FabianoRF <freis884@gmail.com>
2020-10-21 18:24:09 -0300

    estrutura inicial configurada

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/postages', (request, response) => {
+  response.send('Hello world');
+});
+
+app.listen(3333, () => {
+  console.log('*** Server started on port 3333!');
+});
